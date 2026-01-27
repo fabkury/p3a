3b3f7e31ae3b2a098a4211ddf20ad82cabe09056
Fabrício Kury <fab@kury.dev>
2026-01-27 18:22:57 -0500

    Fix formatting issue in makapix_channel_check_and_clear_refresh_immediate function by removing unnecessary newline and ensuring proper function declaration.

--- components/channel_manager/makapix_channel_events.c
+++ components/channel_manager/makapix_channel_events.c
@@ -456,9 +456,7 @@ void makapix_channel_signal_refresh_immediate(void)
 
     ESP_LOGI(TAG, "Signaling immediate channel refresh requested");
     xEventGroupSetBits(s_mqtt_event_group, MAKAPIX_EVENT_REFRESH_IMMEDIATE);
-}
-
-bool makapix_channel_check_and_clear_refresh_immediate(void)
+}bool makapix_channel_check_and_clear_refresh_immediate(void)
 {
     if (!s_mqtt_event_group) {
         return false;
